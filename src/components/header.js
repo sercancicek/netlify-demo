ccd9ad5eaa7a4b777adce1ba997a2358a78e6582
sercancicek <sercan@puentedev.io>
2019-07-08 16:57:53 +0300

    graphql has been removed from header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,21 +4,21 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import headerStyles from "./header.module.scss"
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <header className={headerStyles.header}>
       <h1>
         <Link className={headerStyles.title} to="/">
-          {data.site.siteMetadata.title}
+          Welcome
         </Link>
       </h1>
       <nav>
