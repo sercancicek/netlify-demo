c08ca4eb5cd6f960ff1c84b266737da856fa5190
sercancicek <sercan@puentedev.io>
2019-07-08 17:19:52 +0300

    some features

--- src/components/header.js
+++ src/components/header.js
@@ -4,21 +4,21 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import headerStyles from "./header.module.scss"
 
 const Header = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
   return (
     <header className={headerStyles.header}>
       <h1>
         <Link className={headerStyles.title} to="/">
-          Welcome
+          {data.site.siteMetadata.title}
         </Link>
       </h1>
       <nav>
