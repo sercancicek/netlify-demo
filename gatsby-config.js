e6779cc5918cff5f6861a611fdb6dffe6a792c4a
sercancicek <sercan@puentedev.io>
2019-07-08 16:31:29 +0300

    hey netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-sass',
+        `gatsby-plugin-netlify-cms`,
         {
             resolve: 'gatsby-source-filesystem',
             options: {
