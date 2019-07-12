c70670bc8dc8c0065a3e9867fd05bc0201994144
sercancicek <sercan@puentedev.io>
2019-07-12 15:15:06 +0300

    move

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CMS from 'netlify-cms'
-import IndexComponent from '../../src/templates';
+import IndexComponent from '../templates';
 
 CMS.registerPreviewTemplate('home-page', ({ entry }) => (
   <IndexComponent {...entry.toJS().data} />
