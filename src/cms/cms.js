1db3cb2581f04d88aae1b26abb508475b3736770
sercancicek <sercan@puentedev.io>
2019-07-16 17:00:21 +0300

    css update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 import IndexComponent from '../templates';
-import Layout from '../components/layout';
 
 CMS.registerPreviewTemplate('home', ({ entry }) => {
   console.log({ bb: entry });
   return (
-    <Layout>
-      <IndexComponent {...entry.toJS()} />
-    </Layout>
+    <IndexComponent {...entry.toJS()} />
   )
 })
