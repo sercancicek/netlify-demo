d0b224449d9ae7574297518cd92c5e060f99f2da
sercancicek <sercan@puentedev.io>
2019-07-16 16:55:04 +0300

    layout also has been added to preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 import IndexComponent from '../templates';
+import Layout from '../components/layout';
 
 CMS.registerPreviewTemplate('home', ({ entry }) => {
   console.log({ bb: entry });
   return (
-    <IndexComponent {...entry.toJS()} />
+    <Layout>
+      <IndexComponent {...entry.toJS()} />
+    </Layout>
   )
 })
